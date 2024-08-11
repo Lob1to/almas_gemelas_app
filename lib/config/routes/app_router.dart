2a9def26c8a82689e8a9f1adec7acfb6e17c8a3c
David Lobo <kloboplata@gmail.com>
2024-08-11 14:24:14 -0500

    Approuter configured to protect routes with auth

--- lib/config/routes/app_router.dart
+++ lib/config/routes/app_router.dart
@@ -1,34 +1,64 @@
+import 'package:almas_gemelas_app/config/routes/app_router_notifier.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
+import '../../features/auth/presentation/providers/auth_provider.dart';
 import '../../features/auth/presentation/screens/screens.dart';
 import '../../features/home/presentation/screens/screens.dart';
 
-final appRouterProvider = Provider<GoRouter>((ref) {
-  return GoRouter(
-    initialLocation: '/splash',
-    routes: [
-      ///* Home Screens
-      GoRoute(
-        path: '/',
-        builder: (context, state) => const HomeScreen(),
-      ),
+final appRouterProvider = Provider<GoRouter>(
+  (ref) {
+    final goRouterNotifier = ref.read(goRouterNotifierProvider);
 
-      ///* Auth Screens
-      GoRoute(
-        path: '/splash',
-        builder: (context, state) => const CheckAuthStatusScreen(),
-      ),
+    return GoRouter(
+        initialLocation: '/splash',
+        refreshListenable: goRouterNotifier,
+        routes: [
+          ///* Home Screens
+          GoRoute(
+            path: '/',
+            builder: (context, state) => const HomeScreen(),
+          ),
 
-      GoRoute(
-        path: '/login',
-        builder: (context, state) => const LoginScreen(),
-      ),
+          ///* Auth Screens
+          GoRoute(
+            path: '/splash',
+            builder: (context, state) => const CheckAuthStatusScreen(),
+          ),
 
-      GoRoute(
-        path: '/register',
-        builder: (context, state) => const RegisterScreen(),
-      ),
-    ],
-  );
-});
+          GoRoute(
+            path: '/login',
+            builder: (context, state) => const LoginScreen(),
+          ),
+
+          GoRoute(
+            path: '/register',
+            builder: (context, state) => const RegisterScreen(),
+          ),
+        ],
+        redirect: (context, state) {
+          final isGoingTo = state.fullPath;
+          final authStatus = goRouterNotifier.authStatus;
+
+          if (isGoingTo == '/splash' && authStatus == AuthStatus.checking) {
+            return null;
+          }
+
+          if (authStatus == AuthStatus.notAuthenticated) {
+            if (isGoingTo == '/login' || isGoingTo == '/register') return null;
+
+            return '/login';
+          }
+
+          if (authStatus == AuthStatus.authenticated) {
+            if (isGoingTo == '/login' ||
+                isGoingTo == '/register' ||
+                isGoingTo == '/splash') {
+              return '/';
+            }
+          }
+
+          return null;
+        });
+  },
+);
